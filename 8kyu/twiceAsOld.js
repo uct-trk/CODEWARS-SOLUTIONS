a96848db98faf7b3871254a9171eba04a4a751f4
uğurcan türk <ugurcan.uut82@gmail.com>
2021-12-25 13:28:03 +0300

    twice as old

---
+++ 8kyu/twiceAsOld.js
@@ -0,0 +1,10 @@
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let esitlik = dadYearsOld - sonYearsOld * 2
+    return esitlik < 0 ? esitlik * -1 : esitlik
+  }
+
+ console.log(twiceAsOld(55, 30))
